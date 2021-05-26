0fb0ba49842da043b9bfb860b77a15afe3726413
Levi-coder07 <82822546+Levi-coder07@users.noreply.github.com>
2021-05-26 12:22:35 -0500

    Add files via upload

---
+++ Alumno.java
@@ -0,0 +1,23 @@
+package com.mypage.com;
+
+public class Alumno {
+	private String CUI;
+	private String Clave;
+	
+	public Alumno() {
+		
+	}
+	public String getCUI() {
+		return CUI;
+	}
+	public void setCUI(String cUI) {
+		CUI = cUI;
+	}
+	public String getClave() {
+		return Clave;
+	}
+	public void setClave(String ClavE) {
+		Clave = ClavE;
+	}
+	
+}
